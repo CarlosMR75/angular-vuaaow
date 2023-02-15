27ab20aa5ed9e64222c072d2e307b6dad8c272da
unknown <carlosemr75@gmail.com>
2023-02-15 11:15:36 -0600

    commit 15F

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  {path: '', component:AppComponent, 'pathMatch': 'full'},
-  {path: 'home', component:HomeComponent, 'pathMatch': 'full' }.
+  {path: '', component:HomeComponent, 'pathMatch': 'full'},
   {path: 'login', component:LoginComponent, 'pathMatch': 'full'},
   {path: 'register', component:RegisterComponent, 'pathMatch': 'full'}
 ];
